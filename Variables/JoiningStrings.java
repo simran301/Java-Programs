cba117ba4162b9113062a55e32831b4bec5018f6
Simran Rawat <93734305+simran301@users.noreply.github.com>
2023-03-01 01:52:38 +0530

    Add files via upload

---
+++ Variables/JoiningStrings.java
@@ -0,0 +1,13 @@
+// Strings - Joining Strings
+
+public class JoiningStrings 
+{
+   public static void main(String[] args)
+   {
+     int year= 2010;
+     String winner = "Spain";
+
+     String announcement = "The winner of the " + year + " world cup is " + winner;
+     System.out.println(announcement);
+   }   
+}
