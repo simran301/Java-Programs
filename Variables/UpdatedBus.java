42f46b52aa6b29c8523d782d227ab8bc92140ec5
Simran Rawat <93734305+simran301@users.noreply.github.com>
2023-02-28 23:17:31 +0530

    Add files via upload

---
+++ Variables/UpdatedBus.java
@@ -0,0 +1,17 @@
+//We can update the program & the updated program will be as below:-
+
+public class UpdatedBus
+{
+    public static void main(String[] args)
+    {
+        int passengers = 0;
+        passengers += 9;
+        System.out.println(passengers);
+        
+        passengers -= 5;
+        System.out.println(passengers);
+
+        passengers -= 4;
+        System.out.println(passengers);
+    }
+} 
