062685c4b3a01ece184d0094530cd900af166c76
Simran Rawat <93734305+simran301@users.noreply.github.com>
2023-02-28 19:57:00 +0530

    Add files via upload

---
+++ Patterns.java
@@ -0,0 +1,13 @@
+public class Patterns
+{
+ public static void main(String[] args) 
+ {
+   System.out.println(" SSSS   II  M       M  II");
+   System.out.println("S    S  II  M M   M M  II");  
+   System.out.println("S       II  M  M M  M  II");
+   System.out.println(" SSSS   II  M   M   M  II");
+   System.out.println("     S  II  M       M  II");
+   System.out.println("S    S  II  M       M  II");
+   System.out.println(" SSSS   II  M       M  II");
+ }   
+}
