22771fbb893692c41e26b3afa61dddfd860d05a5
Simran Rawat <93734305+simran301@users.noreply.github.com>
2023-03-01 12:21:44 +0530

    Add files via upload

---
+++ Variables/WholeNumber.java
@@ -0,0 +1,15 @@
+public class WholeNumber 
+{
+    public static void main(String[] args) 
+    {
+        long globalPopulation=8000000000L;
+
+        System.out.println("The global population is " + globalPopulation);
+
+        long dailyGoogleSearches=8500000000L;
+
+        System.out.println("Daily there are " + dailyGoogleSearches + " google searches");
+    }    
+}
+
+// Here in the above program it is really hard to read the digits or we can say it's really hard to read Zeros. What we can do is we can put underscore before every 3 zeros & these underscores doesn,t change anything.
