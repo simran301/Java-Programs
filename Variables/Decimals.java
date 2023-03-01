47f5962db771f0f909b81cbae99fb1c4e352add6
Simran Rawat <93734305+simran301@users.noreply.github.com>
2023-03-01 12:58:43 +0530

    Delete Decimals.java

--- Variables/Decimals.java
+++
@@ -1,8 +0,0 @@
-public class Decimals 
-{
-    public static void main(String[] args)
-    {
-        double percentage=73.5;
-        System.out.println(percentage + " of percntage are madeup");
-    }    
-}
