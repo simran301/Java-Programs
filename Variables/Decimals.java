28b83514d4e07c24e8caadd5a26551f10bfd4194
Simran Rawat <93734305+simran301@users.noreply.github.com>
2023-03-01 12:57:59 +0530

    Add files via upload

---
+++ Variables/Decimals.java
@@ -0,0 +1,8 @@
+public class Decimals 
+{
+    public static void main(String[] args)
+    {
+        double percentage=73.5;
+        System.out.println(percentage + " of percntage are madeup");
+    }    
+}
