46b37e14d10af27dd5ca79d7b2e12bc7a5ab5735
Simran Rawat <93734305+simran301@users.noreply.github.com>
2023-03-01 10:36:58 +0530

    Add files via upload

---
+++ Variables/StringGrades.java
@@ -0,0 +1,13 @@
+public class StringGrades 
+{
+    public static void main(String[] args)
+    {
+        String potions = "A";
+        String arithmancy = "B";
+        String charms = "C";
+
+        System.out.println("Potions " + potions);
+        System.out.println("Arithmancy " + arithmancy);
+        System.out.println("Charms " + charms);
+    }    
+}
