8b9e2000d59ae14be9d671f5751f2832fa47fb39
Simran Rawat <93734305+simran301@users.noreply.github.com>
2023-03-01 10:45:00 +0530

    Add files via upload

---
+++ Variables/StringGrades.java
@@ -0,0 +1,15 @@
+// We can also write CharachterGrades.java program using string data type but in string we need to warp the text using double quotes
+
+public class StringGrades 
+{
+    public static void main(String[] args)
+    {
+        String potions = "A";
+        String arithmancy = "B";
+        String charms = "C";
+
+        System.out.println("Potions " + potions);
+        System.out.println("Arithmancy " + arithmancy);
+        System.out.println("Charms " + charms);
+    }    
+}
