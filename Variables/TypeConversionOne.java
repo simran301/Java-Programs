6f84694e45a15d28f7729b2dcc25e88ed8f8e954
Simran Rawat <93734305+simran301@users.noreply.github.com>
2023-03-02 13:22:17 +0530

    Add files via upload

---
+++ Variables/TypeConversionOne.java
@@ -0,0 +1,12 @@
+public class TypeConversionOne
+{
+    public static void main(String[] args)
+    { 
+        double salary = 5423.94;
+        int roundedSalary = (int)salary;
+
+        System.out.println("Mortgage Borker: What is your montly salary in dollar?");
+
+        System.out.println("My monthly salary is " +roundedSalary);
+    }
+}
